0fcbd9f9d2aae3cd0a4010ac57cae7b1be4609b0
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-14 08:10:42 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 2. Escribir un programa que calcule el área de un rectángulo:
-    // lado1 = 3 lado2 = 4 área del rectángulo= lado1 * lado2
+    // 3. Un programa que lea 4 números y calcule la media.
+    // Media= (num1 + num2 + num3 + num4)/4 
 
-    let lado1 = Number(prompt("El valor de lado 1 es:" , 2))
-    let lado2 = Number(prompt("El valor de lado 2 es:" , 4))
+    let num1 = Number(prompt("Digite el valor del número 1:"))
+    let num2 = Number(prompt("Digite el valor del número 2:"))
+    let num3 = Number(prompt("Digite el valor del número 3:"))
+    let num4 = Number(prompt("Digite el valor del número 4:"))
 
-    console.log(`El área de el rectángulo cuyos lados son 2 y 4 es: ${lado1*lado2}`);
+    console.log(`El caclculo de la media es: ${(num1+num2+num3+num4)/4}`);
 
 })
