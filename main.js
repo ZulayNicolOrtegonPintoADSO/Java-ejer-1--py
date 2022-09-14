5103167c0a1f900a6baf412cde44b5605120fc87
Zulay Ortegon <zulyortegon15@gmail.com>
2022-09-14 17:17:25 -0500

    Ejercicio 16

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 15. Hacer un diagrama para convertir de grados centígrados a grados Fahrenheit.
+    // 16. Elaborar un algoritmo para calcular el promedio final de la materia de algoritmos. Dicha calificación se compone de los siguientes porcentajes:
+    // • 55% del promedio final de sus calificaciones de los tres(3) parciales.
+    // • 30% de la calificación examen final y
+    // • 15% de la calificación trabajo final.
 
-    let c = Number(prompt("Ingrese el valor de grados centigrados:"))
+    let nota1 = Number(prompt("Ingresa el valor de tu nota 1 :"))
+    let nota2 = Number(prompt("Ingresa el valor de tu nota 2 :"))
+    let nota3 = Number(prompt("Ingresa el valor de tu nota 3 :"))
     
-    farenheit=(c*(9/5))+ 32
+    totalparcial=(nota1+nota2+nota3)/3
+    porcen1=totalparcial*0.55
 
-    console.log(`Tus grados c convertidos a farenheit: ${farenheit}`);
+    let nota4 = Number(prompt("Ingresa el valor de tu nota 4 :"))
+
+    porcen2=nota4*0.30
+  
+    let nota5 = Number(prompt("Ingresa el valor de tu nota 5 :"))
+
+    porcen3=nota5*0.15
+    porfin=porcen1+porcen2+porcen3
+
+    console.log(`El porcentaje final es: ${porfin}`);
 
 })
