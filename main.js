e8d7df9e4165b8128a5426b42ff14c01b156307d
Zulay Ortegon <zulyortegon15@gmail.com>
2022-09-14 17:48:19 -0500

    Ejercicio 19

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 18. Calcular el sueldo de un empleado dados comodatos de entrada: el nombre, hrs. De trabajo y el pago en hora. Pagohora=15300
+    // 19. Un estudiante realiza cuatro exámenes. Realizar el pseudocódigo que representen el algoritmo correspondiente para obtener el promedio de las calificaciones obtenidas. las calificaciones van 1 a 5 puntos.
 
-    let nombre =prompt("Nombre:")
+    let examen1 = Number(prompt("Nota del examen 1:"))
+    let examen2 = Number(prompt("Nota del examen 2:"))
+    let examen3 = Number(prompt("Nota del examen 3:"))
+    let examen4 = Number(prompt("Nota del examen 4:"))
 
-    let horas = Number(prompt("Cantidad horas:"))
+    nota=(examen1+examen2+examen3+examen4)/4
 
-    totalapagar=horas*15300
+    if (nota >= 0 && nota < 2.5 ){
+        console.log(`NO APROBÓ`)
+    }
+    
+    if (nota == 2.5 && nota <= 5){
+        console.log(`APROBÓ`);
+    }
 
-    console.log(`sr/sra ${nombre}, el  total de pago por sus horas de trabajo es: ${totalapagar}`);
+    if (nota > 5 ){
+        console.log(`ERROR:Las notas ingresadas no están en el rango de calificación de 1 a 5`);
+    }
 
 })
