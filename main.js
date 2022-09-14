fe0f3ec4d857dd2ac5beddeba6702aa21e38d2e3
Zulay Ortegon <zulyortegon15@gmail.com>
2022-09-14 17:22:47 -0500

    Ejercicio 17

--- main.js
+++ main.js
@@ -1,25 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 16. Elaborar un algoritmo para calcular el promedio final de la materia de algoritmos. Dicha calificación se compone de los siguientes porcentajes:
-    // • 55% del promedio final de sus calificaciones de los tres(3) parciales.
-    // • 30% de la calificación examen final y
-    // • 15% de la calificación trabajo final.
+    // 17. Dado el valor que un cliente paga por un producto, calcular qué valor corresponde al costo total del producto y cuánto es el valor del IVA. Considerando que el porcentaje del IVA puede variar en el tiempo y de un producto a otro, este dato se lee por teclado.
 
-    let nota1 = Number(prompt("Ingresa el valor de tu nota 1 :"))
-    let nota2 = Number(prompt("Ingresa el valor de tu nota 2 :"))
-    let nota3 = Number(prompt("Ingresa el valor de tu nota 3 :"))
-    
-    totalparcial=(nota1+nota2+nota3)/3
-    porcen1=totalparcial*0.55
+    let producto = Number(prompt("valor del producto:"))
 
-    let nota4 = Number(prompt("Ingresa el valor de tu nota 4 :"))
+    let iva = Number(prompt("valor iva:"))
 
-    porcen2=nota4*0.30
-  
-    let nota5 = Number(prompt("Ingresa el valor de tu nota 5 :"))
+    costo=((producto*iva)/100)+producto
 
-    porcen3=nota5*0.15
-    porfin=porcen1+porcen2+porcen3
-
-    console.log(`El porcentaje final es: ${porfin}`);
+    console.log(`valor total: ${costo}`);
 
 })
