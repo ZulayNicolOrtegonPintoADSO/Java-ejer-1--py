7c5a509f6a6a596c3b27f6be884523719485ff71
Zulay Ortegon <zulyortegon15@gmail.com>
2022-09-14 16:56:21 -0500

    Ejercicio 14

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 13. Realice un algoritmo que a partir de proporcionarle la velocidad de un automóvil expresada en kilómetros por hora, proporcione la velocidad en metros por segundos
+    // 14. Una farmacia aplica al precio de los remedios el 10% de descuento, hacer un programa que ingresando el costo de los medicamentos calcules el descuento y el precio final.
 
-    let velocidad = Number(prompt("Escribe aqui la velocidad en km/h"))
-    convertir=velocidad/3.6
 
-    console.log(`Tu velocidad de km/h convertida a m/seg es igual a: ${convertir}`);
+    let precio = Number(prompt("Ingresa el precio:"))
+    
+    descuento=(precio*10)/100
+    preciocondescuento=precio-descuento
+
+    console.log(`El precio con descuento es: ${preciocondescuento}`);
 
 })
